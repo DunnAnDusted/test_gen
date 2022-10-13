358edc4c5f81c8e68a708fa0779c711df256549c
Andrew Dunn <andrew.dunn@ajja.org.uk>
2022-10-13 17:56:06 +0100

    src/lib.rs: Add UR, AI Case + Annotations
    Once again, I failed to account for a potential pattern for a case, in
    this case, unique return types for each case, with all cases being
    ignored.
    
    On-top of this addition, this also marks the addition
    of annotations for the blocks of cases, as well as each unique case,
    to improve the ease of identifying the intended behaviour of the cases
    they should produce, as an attempt to improve maintainability.

--- src/lib.rs
+++ src/lib.rs
@@ -2,51 +2,103 @@
 
 #[macro_export]
 macro_rules! test_gen {
+    //////////////////
+    // Result Cases //
+    //////////////////
+
+    // Ignore All, Unit-Unit
     (ignore, $helper:expr, <_, _> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { ignore, $helper, <(), ()> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // Ignore All, Unit-User Defined
     (ignore, $helper:expr, <_, $err:ty> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { ignore, $helper, <(), $err> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // Ignore All, User Defined-Unit
     (ignore, $helper:expr, <$ok:ty, _> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { ignore, $helper, <$ok, ()> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // Ignore All, User Defined-User Defined
     (ignore, $helper:expr, <$ok:ty, $err:ty> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { ignore, $helper, Result<$ok, $err> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // Unit-Unit
     ($helper:expr, <_, _> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { $helper, <(), ()> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // Unit-User Defined
     ($helper:expr, <_, $err:ty> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { $helper, <(), $err> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // User Define-Unit
     ($helper:expr, <$ok:ty, _> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { $helper, <$ok, ()> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // User Defined-User Defined
     ($helper:expr, <$ok:ty, $err:ty> => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { $helper, Result<$ok, $err> => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    ////////////////////
+    // No/Unit Return //
+    ////////////////////
+
+    // All Ignore, All Should Panic
     (ignore, should_panic, $helper:expr => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { should_panic, ignore, $helper => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    // All Should Panic, All Ignore
     (should_panic, ignore, $helper:expr => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { ignore, $helper, () => { $($case_name: { ($($case_args),+), [should_panic $(, $($attr),+)?] }),+ } }
     };
+
+    // All Should Panic
     (should_panic, $helper:expr => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { $helper => { $($case_name: { ($($case_args),+), [should_panic $(, $($attr),+)?] }),+ } }
     };
+
+    // All Ignore
     (ignore, $helper:expr => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { ignore, $helper, () => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
-    (ignore, $helper:expr, $return:ty => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
-        $crate::test_gen! { $helper => { $($case_name: { $return, ($($case_args),+), [ignore $(, $($attr),+)?] }),+ } }
-    };
-    ($helper:expr, $return:ty => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
-        $crate::test_gen! { $helper => { $($case_name: { $return, ($($case_args),+) $(, [$($attr),+])? }),+ } }
-    };
+
+    // None
     ($helper:expr => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $crate::test_gen! { $helper, () => { $($case_name: { ($($case_args),+) $(, [$($attr),+])? }),+ } }
     };
+
+    ////////////////
+    // All Return //
+    ////////////////
+
+    // All Ignore
+    (ignore, $helper:expr, $return:ty => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
+        $crate::test_gen! { $helper => { $($case_name: { $return, ($($case_args),+), [ignore $(, $($attr),+)?] }),+ } }
+    };
+
+    // None
+    ($helper:expr, $return:ty => { $($case_name:ident: { ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
+        $crate::test_gen! { $helper => { $($case_name: { $return, ($($case_args),+) $(, [$($attr),+])? }),+ } }
+    };
+
+    ///////////////////
+    // Unique Return //
+    ///////////////////
+
+    // All Ignore
+    (ignore, $helper:expr => { $($case_name:ident: { $return:ty, ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
+        $crate::test_gen! { $helper => { $($case_name: { $return, ($($case_args),+), [ignore $(, $($attr),+)?] }),+ } }
+    };
+
+    // BASE
     ($helper:expr => { $($case_name:ident: { $return:ty, ($($case_args:expr),+) $(, [$($attr:meta),+])? }),+ }) => {
         $(
             $($(#[$attr])+)?
