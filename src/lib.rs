7180e665571c75f2d91502e38a6b3f4b6e558806
Andy <andrew.dunn@ajja.org.uk>
2022-11-28 23:52:20 +0000

    feat(lib): Reimplement test_gen! as a proc-macro
    Having been experimenting with proc-macros, this commit reimplements
    `test_gen`, previously implemented as a declarative macro
    using `macro_rules!`, as a proc-macro, using the `syn`, `quote` and
    `proc-macro2` libraries.
    
    This includes a number of behavioural changes, as well as syntax
    changes, such as the switch to full attribute syntax, a keyword
    separator between attributes and the helper function, in order to
    provide more helpful errors in the case of mangled attribute syntax,
    switching to a `Path` value for the helper function, baring the use of
    closures as arguments, the option for static arguments, which will be
    passed to the helper function for every test case, and specifying a
    default return type for the helper function, as well as case specific
    types which will override it, rather than one or the other...
    
    BREAKING CHANGE:
    
    * Attributes now specified by full annotation, rather than array
    * A keyword separator is required before the helper function.

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,18 @@
 //! See [LICENSE] for details.
 //!
 //! [LICENSE]: https://github.com/DunnAnDusted/test_gen/blob/main/LICENSE
-#![no_std]
 #![warn(missing_docs)]
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::ToTokens;
+use syn::{
+    braced, parenthesized,
+    parse::{Parse, ParseStream, Result},
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    token::{Brace, Colon, Comma, FatArrow, Paren, RArrow},
+    Attribute, Error, Expr, Ident, Path, Token, Type,
+};
 
 /// Generates unique, named test cases, based on parameterized inputs.
 ///
@@ -74,73 +84,424 @@
 /// isn't supported by Rust's testing harness for these cases.)
 ///
 /// [`Termination`]: https://doc.rust-lang.org/std/process/trait.Termination.html
-#[macro_export]
-macro_rules! test_gen {
-    ////////////////////
-    // All Case Attrs //
-    ////////////////////
+#[proc_macro]
+pub fn test_gen(tokens: TokenStream) -> TokenStream {
+    parse_macro_input!(tokens as TestHelper)
+        .restructure()
+        // `proc_macro2::TokenStream` needs to be converted back
+        // to `proc_macro::TokenStream` before it can be output
+        .into()
+}
 
-    // All Ignore, All Should Panic
-    ([ignore, should_panic], $helper:expr => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) }),+ }) => {
-        test_gen! { [should_panic, ignore], $helper => { $($case_name: { $([$($attr),+], )? ($($case_args),+) }),+ } }
-    };
+/// The main type, representing the collective structure of `test_gen`.
+///
+/// This includes:
+///
+/// * Attributes to apply to every test case
+/// * The separator before the helper function (for preserving spans)
+/// * The helper function for driving the tests
+/// * Arguments to pass to the helper function for every test
+/// * The default return type for the helper function
+/// * Formatting elements (for preserving spans)
+/// * The values for producing the resulting test
+#[derive(Clone)]
+struct TestHelper {
+    static_attrs: Vec<Attribute>,
+    separator: Separator, // Preserved for span
+    helper: Path,
+    static_args: Option<FnArgs>,
+    static_return_type: Option<ReturnType>,
+    farrow: FatArrow, // Preserved for span
+    braces: Brace,    // Preserved for span
+    cases: Punctuated<TestCase, Comma>,
+}
 
-    // All Should Panic, All Ignore
-    ([should_panic, ignore], $helper:expr => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) }),+ }) => {
-        test_gen! { [ignore], $helper => { $($case_name: { [should_panic $(, $($attr),+)?], ($($case_args),+)}),+ } }
-    };
+impl TestHelper {
+    /// Consumes the `TestHelper`, and constructs the token stream of the resulting test cases.
+    fn restructure(self) -> TokenStream2 {
+        let Self {
+            static_attrs,
+            separator: _,
+            helper,
+            mut static_args,
+            static_return_type,
+            farrow: _,
+            braces: _,
+            cases,
+        } = self;
 
-    // All Should Panic
-    ([should_panic], $helper:expr => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) }),+ }) => {
-        test_gen! { $helper => { $($case_name: { [should_panic $(, $($attr),+)?], ($($case_args),+) }),+ } }
-    };
+        // Appends trailing punctuation when static args are specified,
+        // to ensure it doesn't conflict with the case specific args,
+        // and maps away the parens.
+        let static_args = static_args.as_mut().map(|x| {
+            x.args.push_punct(Default::default());
+            &x.args
+        });
 
-    ////////////////
-    // All Return //
-    ////////////////
+        // Mapped to an immutable referance, to ensure the value
+        // isn't moved/consumed.
+        let static_return_type = static_return_type.as_ref();
 
-    // All Ignore
-    ([ignore], $helper:expr => $return:ty => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) }),+ }) => {
-        test_gen! { $helper => { $($case_name: { [ignore $(, $($attr),+)?], ($($case_args),+) => $return }),+ } }
-    };
+        cases
+            .into_iter()
+            .map(|case| -> TokenStream2 {
+                let TestCase {
+                    fn_name,
+                    colon: _,
+                    args:
+                        CaseArgs {
+                            braces: _,
+                            attrs,
+                            args,
+                            return_type,
+                        },
+                } = case;
 
-    // None
-    ($helper:expr => $return:ty => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) }),+ }) => {
-        test_gen! { $helper => { $($case_name: { $([$($attr),+], )? ($($case_args),+) => $return }),+ } }
-    };
+                let args = &args.args;
+                // Specifies the default return type as an alternate value.
+                let return_type = return_type.as_ref().or(static_return_type);
 
-    ///////////////////
-    // Unique Return //
-    ///////////////////
+                // #(#VAR)* syntax behaves similarly to `macro_rules!` equivilent,
+                // for items implementing `IntoIterator<Item: ToTokens>`.
+                //
+                // `return_type` can be referanced directly,
+                // due to `ToTokens` being implemented for `Option<T: ToTokens>`,
+                // and `ReturnType` including the `->` as part of its `ToTokens` implementation.
+                //
+                // Separating comma already added to `static_args`,
+                // ensuring it's conditional inclusion.
+                parse_quote! {
+                    #(#static_attrs)*
+                    #(#attrs)*
+                    #[test]
+                    fn #fn_name() #return_type {
+                        #helper(#static_args #args)
+                    }
+                }
+            })
+            .collect()
+    }
+}
 
-    // All Ignore
-    ([ignore], $helper:expr => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) $(=> $return:ty)? }),+ }) => {
-        test_gen! { $helper => { $($case_name: { [ignore $(, $($attr),+)?], ($($case_args),+) $(=> $return)? }),+ } }
-    };
+impl Parse for TestHelper {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // Parses any attributes to apply to all test cases.
+        let static_attrs = input.call(Attribute::parse_outer)?;
 
-    // BASE
-    ($helper:expr => { $($case_name:ident: { $([$($attr:meta),+], )? ($($case_args:expr),+) $(=> $return:ty)? }),+ }) => {
-        $(
-            $($(#[$attr])+)?
-            #[test]
-            fn $case_name() $(-> $return)? {
-                $helper($($case_args),+)
-            }
-        )+
-    };
+        // TODO: A separator preceeding the helper function specification, isn't a great solution,
+        // style wise, but does promote a potentially more helpful error message
+        // in the case of malformed syntax when specifying attributes...
+        let separator = input.parse().map_err(|err| {
+            Error::new(err.span(), "expected attributes, `fn`, `struct` or `enum`")
+        })?;
+
+        // NOTE: Whilst designed with the intent of specifying a helper *function*,
+        // an oddity of the syntax for `test_gen`, means it's also compatible
+        // with tuple structs and enum varients! Oop! xD
+        let helper = input
+            .parse()
+            .map_err(|err| Error::new(err.span(), "expected helper function"))?;
+        let static_args = input.peek(Paren).then(|| input.parse()).transpose()?;
+        let static_return_type = ReturnType::try_parse(input).transpose()?;
+        let farrow = input.parse::<Token![=>]>()?;
+        let cases;
+        let braces = braced!(cases in input);
+        // If the contents of the`cases` is empty,
+        // `ParseBuffer::parse_terminated` will simply produce an empty
+        // `Punctuated` struct, and no error. On the other hand,
+        // `Punctuated::parse_separated_nonempty` falls back on the parsing error
+        // for `TestCase`, as well as not allow trailing commas,
+        // neither of which is particularly helpful in this case...
+        //
+        // Instead, its explicitly checked whether `cases` is empty,
+        // resulting in a bespoke error which provides an explanation which is actually
+        // helpful.
+        let cases = cases
+            .is_empty()
+            .then(|| Error::new(cases.span(), "expected test cases"))
+            .map(Result::Err) // Wrapping the error with a `map` format more cleanly...
+            .unwrap_or_else(|| cases.parse_terminated(TestCase::parse))?;
+
+        let out = TestHelper {
+            static_attrs,
+            separator,
+            helper,
+            static_args,
+            static_return_type,
+            farrow,
+            braces,
+            cases,
+        };
+
+        Ok(out)
+    }
+}
+
+impl ToTokens for TestHelper {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        // `ToTokens` isn't implemented for anything like `&[T: ToTokens]`
+        // or `(Into)Iterator<Item: ToTokens>`, so appending them iteratively
+        // is about as clean a solution as possible...
+        self.static_attrs
+            .iter()
+            .for_each(|attr| attr.to_tokens(tokens));
+        self.separator.to_tokens(tokens);
+        self.helper.to_tokens(tokens);
+        self.static_args.to_tokens(tokens);
+        self.static_return_type.to_tokens(tokens);
+        self.farrow.to_tokens(tokens);
+        // Token groups are kind of weird, so uses `surround` to identify the tokens which the
+        // group should surround...
+        self.braces
+            .surround(tokens, |inner| self.cases.to_tokens(inner));
+    }
+}
+
+/// A type representing a separator between the attributes applied to a block of tests,
+/// and the helper "function" used (tuple structs and tuple enum variants are also valid, so this
+/// type allows for those...)
+#[derive(Clone)]
+enum Separator {
+    Fn(Token![fn]),
+    Struct(Token![struct]),
+    Enum(Token![enum]),
+}
+
+impl Parse for Separator {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+
+        if lookahead.peek(Token![fn]) {
+            input.parse().map(Self::Fn)
+        } else if lookahead.peek(Token![struct]) {
+            input.parse().map(Self::Struct)
+        } else if lookahead.peek(Token![enum]) {
+            input.parse().map(Self::Enum)
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+impl ToTokens for Separator {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        match self {
+            Self::Fn(item) => item.to_tokens(tokens),
+            Self::Struct(item) => item.to_tokens(tokens),
+            Self::Enum(item) => item.to_tokens(tokens),
+        }
+    }
+}
+
+/// A type representing an specific test case.
+///
+/// This includes:
+///
+/// * The name it will be assigned
+/// * Formatting elements (for preserving spans)
+/// * The arguments to specify its behaviour (see `CaseArgs` for more detail)
+#[derive(Clone)]
+struct TestCase {
+    fn_name: Ident,
+    colon: Colon,
+    args: CaseArgs,
+}
+
+impl Parse for TestCase {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // Maps to a bespoke error, as noting that you expect a general "Ident",
+        // isn't particularly helpful...
+        let fn_name = input
+            .parse()
+            .map_err(|err| Error::new(err.span(), "expected test case name"))?;
+        let colon = input.parse::<Token![:]>()?;
+        let args = input.parse()?;
+
+        let out = TestCase {
+            fn_name,
+            colon,
+            args,
+        };
+
+        Ok(out)
+    }
+}
+
+impl ToTokens for TestCase {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        self.fn_name.to_tokens(tokens);
+        self.colon.to_tokens(tokens);
+        self.args.to_tokens(tokens);
+    }
+}
+
+/// A type representing values defining the behaviour of a test case.
+///
+/// This includes:
+///
+/// * Formatting elements (for preserving spans)
+/// * Attributes to apply to the specific test case
+/// * The arguments to pass to the helper function for the specific test case
+/// * The expected return type for the specific test case
+#[derive(Clone)]
+struct CaseArgs {
+    braces: Brace,
+    attrs: Vec<Attribute>,
+    args: FnArgs,
+    return_type: Option<ReturnType>,
+}
+
+impl Parse for CaseArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let inner;
+        let braces = braced!(inner in input);
+
+        let attrs = inner.call(Attribute::parse_outer)?;
+
+        // We can actually usefully validate that we're recieving the next token we expect in this
+        // case! Hooray!
+        if !inner.peek(Paren) {
+            return Err(Error::new(
+                inner.span(),
+                "expected attributes or function parameters",
+            ));
+        }
+
+        let args = inner.parse()?;
+        let return_type = ReturnType::try_parse(&inner).transpose()?;
+
+        let out = CaseArgs {
+            braces,
+            attrs,
+            args,
+            return_type,
+        };
+
+        Ok(out)
+    }
+}
+
+impl ToTokens for CaseArgs {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        // All the values for the case are surrounded with braces in this case,
+        // so the tokens are all appended within the body of the closure...
+        self.braces.surround(tokens, |inner| {
+            self.attrs.iter().for_each(|attr| attr.to_tokens(inner));
+            self.args.to_tokens(inner);
+            self.return_type.to_tokens(inner);
+        });
+    }
+}
+
+/// A type representing a list of arguments, and the parentheise around them.
+#[derive(Clone)]
+struct FnArgs {
+    parens: Paren,
+    args: Punctuated<Expr, Comma>,
+}
+
+impl Parse for FnArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let inner;
+        let parens = parenthesized!(inner in input);
+        // Using `punctuated::parse_separated_nonempty` in this case,
+        // because if this type is being parsed, arguments should be expected,
+        // and trailing commas look sloppy...
+        let args = Punctuated::parse_separated_nonempty(&inner)
+            .map_err(|err| Error::new(err.span(), "missing function arguments"))?;
+
+        Ok(Self { parens, args })
+    }
+}
+
+impl ToTokens for FnArgs {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        self.parens
+            .surround(tokens, |inner| self.args.to_tokens(inner))
+    }
+}
+
+/// A type representing the right-arrow and function's return type signature.
+#[derive(Clone)]
+struct ReturnType {
+    arrow: RArrow,
+    return_type: Type,
+}
+
+impl ReturnType {
+    /// Conditionally parses the type, if a right-arrow is peeked from the stream.
+    ///
+    /// Included, due to the optional nature of return types in this macro.
+    fn try_parse(input: ParseStream) -> Option<Result<Self>> {
+        input.peek(Token![->]).then(|| input.parse())
+    }
+}
+
+impl Parse for ReturnType {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let arrow = input.parse()?;
+
+        // Default error message is a bit obtuse in this case,
+        // so it's mapped to a more specific bespoke error instead.
+        let return_type = input
+            .parse()
+            .map_err(|err| Error::new(err.span(), "expected a return type"))?;
+
+        Ok(Self { arrow, return_type })
+    }
+}
+
+impl ToTokens for ReturnType {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        self.arrow.to_tokens(tokens);
+        self.return_type.to_tokens(tokens);
+    }
 }
 
 #[doc = include_str!("../README.md")]
 #[cfg(doctest)]
 struct ReadMeDocTestDummy;
 
-// Basic tests, to confirm the macro can generate `#![no_std]` compatible code.
 #[cfg(test)]
-mod no_std_tests {
-    test_gen! {
-        Into::into => {
-            unit: { (()) },
-            result: { (Ok(())) => Result<(), ()> }
-        }
+mod tests {
+    use super::*;
+
+    fn parse_to_tokens<P: Parse + ToTokens>(p: &'static str) {
+        let tokens = p
+            .parse::<TokenStream2>()
+            .expect("string could not be parsed as tokens")
+            .to_string();
+
+        let parsed = syn::parse_str::<P>(&tokens)
+            .expect("tokens could not be parsed as type")
+            .into_token_stream()
+            .to_string();
+
+        assert_eq!(parsed, tokens);
+    }
+
+    #[test]
+    fn return_type_parsing() {
+        parse_to_tokens::<ReturnType>("-> usize");
+    }
+
+    #[test]
+    fn fn_args_parsing() {
+        parse_to_tokens::<FnArgs>("(1, 2)");
+    }
+
+    #[test]
+    fn case_args_parsing() {
+        parse_to_tokens::<CaseArgs>("{ #[ignore] (1, 2) -> usize }");
+    }
+
+    #[test]
+    fn test_case_parsing() {
+        parse_to_tokens::<TestCase>("test: { #[ignore] (1, 2) -> usize }");
+    }
+
+    #[test]
+    fn test_helper_parsing() {
+        parse_to_tokens::<TestHelper>("#[should_panic] fn Into::into -> (usize, usize) => { test: { #[ignore] (1, 2) -> usize } }");
     }
 }
